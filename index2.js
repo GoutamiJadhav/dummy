4e4c8edf8cfbaf200760395bc7e3c059ed6b34fd
GoutamiJadhav <goutami.jadhav99@gmail.com>
2023-11-02 12:16:32 +0530

    first commit

---
+++ index2.js
@@ -0,0 +1,21 @@
+// how to declare a variable
+let name='mosh';
+console.log(name);
+// cant be a reserved keyword
+// should be meaningful
+// cannont start with a Numbercannot contain a space or hyphen
+//constants in js
+const interest=0.3
+interest=1
+console.log(interest)
+//primitive types-str,boolean,numbers,
+let name='mosh';
+let isaprroved=false
+let age=30;
+let firstname;
+//dynamic typing
+
+//arrays
+let colours=['red','green']
+console.log(colours)
+
